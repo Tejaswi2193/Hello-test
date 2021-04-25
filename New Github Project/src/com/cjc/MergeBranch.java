427c54eb4a586f30476f0d445fe2d99f797e4d63
ASUS <ASUS@LAPTOP-4LAMJMI4>
2021-04-25 16:48:23 +0530

    commit by cjc

---
+++ New Github Project/src/com/cjc/MergeBranch.java
@@ -0,0 +1,19 @@
+package com.cjc;
+
+public class MergeBranch 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+	}
+	public void sum()
+	{
+		System.out.println("Hello sum");
+	}
+		public void sub()
+	{
+		System.out.println("Hello sub");
+	}
+	}
+
+
